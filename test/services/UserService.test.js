0416ecc88d82ad440e404f6da3c6cf0b2914fef9
zenriquez <zenriquez@uabc.edu.mx>
2022-05-11 12:43:59 -0500

    Requirements 3 and 4 sprin 2 done

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -10,7 +10,7 @@ describe ("Test for UserService", () => {
 })
 
 
-describe('2. Get all user data in a list', () => { 
+describe("2. Get all user data in a list", () => { 
     const user = UserService.create(1, "zenriquez", "zabdi")
     const UserInfoInList = UserService.getInfo(user)
     expect (UserInfoInList[0]).toBe(1)
@@ -18,6 +18,22 @@ describe('2. Get all user data in a list', () => {
     expect (UserInfoInList[2]).toBe("zabdi")
     expect (UserInfoInList[3]).toBe("Sin bio")
  })
+
+describe("3. Update username", () => {
+    const user  = UserService.create(1, "zenr", "zabdi")
+    UserService.updateUserUsername(user, "zenr")
+    expect(user.username).toBe("zenr")
+})
+
+
+describe("4. Given a list of users give me the list of usernames", () => {
+    const user1 = UserService.create(1, "zenriquez1", "Zabdi")
+    const user2 = UserService.create(2, "zenriquez2", "Zabdi")
+    const user3 = UserService.create(3,"zenrqiuez3", "Zabdi")
+    expect(usernames).toContain(zenriquez1)
+    expect(usernames).toContain(zenriquez2)
+    expect(usernames).toContain(zenriquez3)
+})
 
 
 })
