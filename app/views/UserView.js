e4cdb731095162a7816eaf46e12d473bd39e96e5
zenriquez <zenriquez@uabc.edu.mx>
2022-05-12 11:25:10 -0500

    requirment 3 and 4 passed

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -6,7 +6,7 @@ class UserView{
         if(payload === null){
             console.log("error null")
             return{error:"Payload doesn't exist."}
-        } else if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number') {
+        } else if(typeof payload.username === 'string' && payload.username == null && typeof payload.name === 'string' && typeof payload.name == null && typeof payload.id === 'number' && typeof payload.id == null) {
         
         }else{
             return {error: "Error: Payload properties need a valid value."}
--- test/views/UserViews.test.js
+++ test/views/UserViews.test.js
@@ -8,4 +8,18 @@ describe("Test for UserView", () => {
         expect(result.error).toMatch(/Payload doesn't exist./)
 
     })
+
+    
+    test("Return an error object when try to create a new user with payload with missing properties",() => {
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/Error: Payload properties need a valid value./)
+    })
 })
+
+
+    test("Return an error object when try to create a new user with a payload  with missing  properties",()=>{
+        const payload = {username:"Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/need a valid value/)
+    })
